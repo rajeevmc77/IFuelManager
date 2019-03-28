a8b27ffb8ff374af11696f4e96d762cded35c9f9
rajeevmc77 <rajeevmc77@gmail.com>
2019-03-28 15:17:21 +0530

    Changes for Fuel Tank Level

--- Server/obdService/views.py
+++ Server/obdService/views.py
@@ -3,21 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import generics
-from .models import CarOBDData, CarJSONOBDData
-from .serializers import CarOBDDataSerializer, CarJSONOBDDataSerializer
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import json
 
-
-# class ListCarsView(generics.ListAPIView):
-#     """
-#     Provides a get method handler.
-#     """
-#     queryset = CarProfile.objects.all()
-#     serializer_class = CarSerializer
+from .models import CarOBDData, CarJSONOBDData, CarProfile
+from .serializers import CarOBDDataSerializer, CarJSONOBDDataSerializer
 
 class CarOBDDataView(APIView):
     """
@@ -38,26 +30,44 @@ class CarOBDDataView(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        print (request.data);
-        d = request.data
-        d["FuelTankLevel"] = 50
-        print (d)
-        serializer = CarOBDDataSerializer(data=request.data)
+        data = self.getJSONFromRawData(request)
+        if data['FuelTankLevel'] == -1:
+            data['FuelTankLevel'] = self.getProjectedRemainingFuel()
+        print (data)
+        serializer = CarOBDDataSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # def getJSONFromRawData(self,request):
-    #     try:
-    #         if isinstance(ele, dict):
-    #         serializer = CarOBDDataSerializer(data=request.data)
-    #
-    #     except:
+    def getJSON(self,request):
+        jsondata = request.body.decode("utf-8").rstrip('\x00')
+        data = json.loads(jsondata)
+        return  data
 
+    def getJSONFromRawData(self,request):
+        try:
+            data = request.data
+            if isinstance(data, dict):
+                if not data:
+                    data = self.getJSON(request)
+            else:
+                data = self.getJSON(request)
+        except:
+            data = self.getJSON(request)
+        return data
 
+    def getProjectedRemainingFuel(self):
+        projectedRemainingFuel = 85
+        previousReading = CarOBDData.objects.values_list('FuelTankLevel').order_by('-created_at')[:1];
+        if not previousReading:
+            previousReading = 81
+        else:
+            previousReading = previousReading[0][0]
+        projectedRemainingFuel = previousReading
+        print (previousReading)
 
-
+        return projectedRemainingFuel;
 
 class CarJSONOBDDataView(APIView):
     """
