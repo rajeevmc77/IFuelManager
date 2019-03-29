07bb202ce60f508800cc9318e487e5d7996f3b03
rajeevmc77 <rajeevmc77@gmail.com>
2019-03-29 13:40:04 +0530

    Changes for Fuel Tank Levelfixed OBD errors

--- Server/dashboard/views.py
+++ Server/dashboard/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
-from obdService.models import CarOBDData
+from obdService.models import CarOBDData, CarProfile
 # Create your views here.
 
 class DashboardView(View):
     def get(self, request):
         # <view logic>
         return render(request, "dashboard/dashboard.html",
-                      { 'CarOBDData': CarOBDData.objects.all()})
+                      { 'CarOBDData': CarOBDData.objects.all(), 'CarProfile': CarProfile.objects.all() })
--- Server/obdService/admin.py
+++ Server/obdService/admin.py
@@ -3,5 +3,8 @@ from .models import CarOBDData, CarProfile
 
 # Register your models here.
 
+admin.site.site_header = "iFuel Manager "
+admin.site.site_title = "iFuel Manager"
+
 admin.site.register(CarOBDData)
 admin.site.register(CarProfile)
