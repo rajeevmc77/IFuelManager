48d908c0cdded6820a9a34c10b541808ec84661d
Rajeev <rajeevmc77@gmail.com>
2019-03-31 14:29:11 +0530

    Added code for FuelSpike detection using simple math

--- Server/dashboard/views.py
+++ Server/dashboard/views.py
@@ -9,8 +9,22 @@ from obdService.models import CarOBDData, CarProfile
 
 class DashboardView(View):
     def get(self, request):
-        #carOBDData = CarOBDData.objects.values('VIN').annotate(Avg('FuelUsageDeviation'))
-        #CarOBDData.objects.aggregate(Avg('FuelUsageDeviation'))
+        carsWithSpuriousHistory = CarOBDData.objects.values('VIN').filter(PossibleFuelLeak=1).distinct()
+        carsWithCleanHistry = CarOBDData.objects.values('VIN').exclude(VIN__in=carsWithSpuriousHistory).distinct()
+        carProfiles = list(CarProfile.objects.all())
+        carHistoryProfile = dict()
+        carsHistry= list()
+        carsHistry.append([dict(item,**{'PossibleFuelLeak':0}) for item in carsWithCleanHistry])
+        carsHistry.append([dict(item, **{'PossibleFuelLeak': 1}) for item in carsWithSpuriousHistory])
+
+        for item in carsHistry:
+            for profile in carProfiles:
+                if profile.VIN == item[0]["VIN"]:
+                    item[0].update({"Make": profile.Make, "Model": profile.Model})
+                    break
 
         return render(request, "dashboard/dashboard.html",
-                      { 'CarOBDData': CarOBDData.objects.all(), 'CarProfile': CarProfile.objects.all() })
+                      { 'CarOBDData': CarOBDData.objects.all(),
+                        'CarProfile': CarProfile.objects.all(),
+                        'CarHistory': carsHistry
+                      })
