33ae25e79d44a83971ffbd16aed0d20aa4333325
Rajeev <rajeevmc77@gmail.com>
2019-03-31 14:39:40 +0530

    Added code for FuelSpike detection using simple math

--- Server/dashboard/views.py
+++ Server/dashboard/views.py
@@ -13,14 +13,14 @@ class DashboardView(View):
         carsWithCleanHistry = CarOBDData.objects.values('VIN').exclude(VIN__in=carsWithSpuriousHistory).distinct()
         carProfiles = list(CarProfile.objects.all())
         carHistoryProfile = dict()
-        carsHistry= list()
-        carsHistry.append([dict(item,**{'PossibleFuelLeak':0}) for item in carsWithCleanHistry])
-        carsHistry.append([dict(item, **{'PossibleFuelLeak': 1}) for item in carsWithSpuriousHistory])
+
+        carsHistry = [dict(item,**{'PossibleFuelLeak':0}) for item in carsWithCleanHistry]
+        carsHistry = carsHistry + [dict(item, **{'PossibleFuelLeak': 1}) for item in carsWithSpuriousHistory]
 
         for item in carsHistry:
             for profile in carProfiles:
-                if profile.VIN == item[0]["VIN"]:
-                    item[0].update({"Make": profile.Make, "Model": profile.Model})
+                if profile.VIN == item["VIN"]:
+                    item.update({"Make": profile.Make, "Model": profile.Model})
                     break
 
         return render(request, "dashboard/dashboard.html",
