c4a26f1ca0e508a92b8f063e9a4d4ff6582e6c01
Rajeev <rajeevmc77@gmail.com>
2019-04-09 12:51:54 +0530

    Added code for Guages

--- Server/dashboard/static/dashboard/app.js
+++ Server/dashboard/static/dashboard/app.js
@@ -1,13 +1,20 @@
+var optsFuelMeter;
+var optsRPMMeter;
+var optsSpeedoMeter;
+
+var guageControlMap = [];
+
+
 function loadAjaxData(){
-    var table = $('#obdData').DataTable();
+    /*var table = $('#obdData').DataTable();
     table.ajax.reload();
 
-    setTimeout(function(){loadAjaxData();}, 1000);
+    setTimeout(function(){loadAjaxData();}, 1000);*/
 }
 
 
 $(document).ready(function() {
-    $('#obdData').DataTable( {
+/*    $('#obdData').DataTable( {
         "ajax": {
             "url": "/api/obdData/",
             "dataSrc": ""
@@ -20,133 +27,154 @@ $(document).ready(function() {
         ]
     });
 
-    loadAjaxData();
+    loadAjaxData();*/
+    console.log(CarFuelHistory);
+    defineGuageOpts();
+    initGuages();
+
 });
 
-var optsFuelMeter = {
-    angle: 0, // The span of the gauge arc
-    lineWidth: 0.2, // The line thickness
-    radiusScale: 1, // Relative radius
-    pointer: {
-        length: 0.5, // // Relative to gauge radius
-        strokeWidth: 0.035, // The thickness
-        color: '#000000' // Fill color
-    },
-     staticLabels: {
-        font: "10px sans-serif",
-        labels: [0,20,40,60,80, 100],
-        fractionDigits: 0
-      },     
-    limitMax: true,     // If false, max value increases automatically if value > maxValue
-    limitMin: true,     // If true, the min value of the gauge will be fixed
-    colorStart: '#6FADCF',   // Colors
-    colorStop: '#8FC0DA',    // just experiment with them
-    strokeColor: '#E0E0E0',  // to see which ones work best for you
-    generateGradient: true,
-    highDpiSupport: true,     // High resolution support
-    // renderTicks is Optional
-    renderTicks: {
-        divisions: 5,
-        divWidth: 1.1,
-        divLength: 0.7,
-        divColor: '#333333',
-        subDivisions: 3,
-        subLength: 0.5,
-        subWidth: 0.6,
-        subColor: '#666666'
-    }                
-};
+function defineGuageOpts(){
+/*
+ This method Initialize the Optional valuex for the Guages
+ Author : Akshara Gireesh Murali
+*/
+      optsFuelMeter = {
+        angle: 0, // The span of the gauge arc
+        lineWidth: 0.2, // The line thickness
+        radiusScale: 1, // Relative radius
+        pointer: {
+            length: 0.5, // // Relative to gauge radius
+            strokeWidth: 0.035, // The thickness
+            color: '#000000' // Fill color
+        },
+         staticLabels: {
+            font: "10px sans-serif",
+            labels: [0,20,40,60,80, 100],
+            fractionDigits: 0
+          },
+        limitMax: true,     // If false, max value increases automatically if value > maxValue
+        limitMin: true,     // If true, the min value of the gauge will be fixed
+        colorStart: '#6FADCF',   // Colors
+        colorStop: '#8FC0DA',    // just experiment with them
+        strokeColor: '#E0E0E0',  // to see which ones work best for you
+        generateGradient: true,
+        highDpiSupport: true,     // High resolution support
+        // renderTicks is Optional
+        renderTicks: {
+            divisions: 5,
+            divWidth: 1.1,
+            divLength: 0.7,
+            divColor: '#333333',
+            subDivisions: 3,
+            subLength: 0.5,
+            subWidth: 0.6,
+            subColor: '#666666'
+        }
+    };
 
-var optsRPMMeter = {
-    angle: 0, // The span of the gauge arc
-    lineWidth: 0.2, // The line thickness
-    radiusScale: 1, // Relative radius
-    pointer: {
-        length: 0.5, // // Relative to gauge radius
-        strokeWidth: 0.035, // The thickness
-        color: '#000000' // Fill color
-    },
-     staticLabels: {
-        font: "10px sans-serif",
-        labels: [0,10,20, 30,40, 50, 60,70],
-        fractionDigits: 0
-      },     
-    limitMax: true,     // If false, max value increases automatically if value > maxValue
-    limitMin: true,     // If true, the min value of the gauge will be fixed
-    colorStart: '#6FADCF',   // Colors
-    colorStop: '#8FC0DA',    // just experiment with them
-    strokeColor: '#E0E0E0',  // to see which ones work best for you
-    generateGradient: true,
-    highDpiSupport: true,     // High resolution support
-    // renderTicks is Optional
-    renderTicks: {
-        divisions: 5,
-        divWidth: 1.1,
-        divLength: 0.7,
-        divColor: '#333333',
-        subDivisions: 3,
-        subLength: 0.5,
-        subWidth: 0.6,
-        subColor: '#666666'
-    }                
-};
+      optsRPMMeter = {
+        angle: 0, // The span of the gauge arc
+        lineWidth: 0.2, // The line thickness
+        radiusScale: 1, // Relative radius
+        pointer: {
+            length: 0.5, // // Relative to gauge radius
+            strokeWidth: 0.035, // The thickness
+            color: '#000000' // Fill color
+        },
+         staticLabels: {
+            font: "10px sans-serif",
+            labels: [0,10,20, 30,40, 50, 60,70],
+            fractionDigits: 0
+          },
+        limitMax: true,     // If false, max value increases automatically if value > maxValue
+        limitMin: true,     // If true, the min value of the gauge will be fixed
+        colorStart: '#6FADCF',   // Colors
+        colorStop: '#8FC0DA',    // just experiment with them
+        strokeColor: '#E0E0E0',  // to see which ones work best for you
+        generateGradient: true,
+        highDpiSupport: true,     // High resolution support
+        // renderTicks is Optional
+        renderTicks: {
+            divisions: 5,
+            divWidth: 1.1,
+            divLength: 0.7,
+            divColor: '#333333',
+            subDivisions: 3,
+            subLength: 0.5,
+            subWidth: 0.6,
+            subColor: '#666666'
+        }
+    };
 
-var optsSpeedoMeter = {
-    angle: 0, // The span of the gauge arc
-    lineWidth: 0.2, // The line thickness
-    radiusScale: 1, // Relative radius
-    pointer: {
-        length: 0.5, // // Relative to gauge radius
-        strokeWidth: 0.035, // The thickness
-        color: '#000000' // Fill color
-    },
-     staticLabels: {
-        font: "10px sans-serif",
-        labels: [0,20, 40,80, 100, 120,140,160,180,200],
-        fractionDigits: 0
-      },
-      staticZones: [
-         {strokeStyle: "#FFDD00", min: 0, max: 40},					 
-         {strokeStyle: "#30B32D", min: 40, max: 60},
-         {strokeStyle: "#F03E3E", min: 60, max: 200}
-      ], 
-    limitMax: true,     // If false, max value increases automatically if value > maxValue
-    limitMin: true,     // If true, the min value of the gauge will be fixed
-    colorStart: '#6FADCF',   // Colors
-    colorStop: '#8FC0DA',    // just experiment with them
-    strokeColor: '#E0E0E0',  // to see which ones work best for you
-    // generateGradient: true,
-    highDpiSupport: true,     // High resolution support
-    // renderTicks is Optional
-    renderTicks: {
-        divisions: 5,
-        divWidth: 1.1,
-        divLength: 0.7,
-        divColor: '#333333',
-        subDivisions: 3,
-        subLength: 0.5,
-        subWidth: 0.6,
-        subColor: '#666666'
-    }                
-};
+      optsSpeedoMeter = {
+        angle: 0, // The span of the gauge arc
+        lineWidth: 0.2, // The line thickness
+        radiusScale: 1, // Relative radius
+        pointer: {
+            length: 0.5, // // Relative to gauge radius
+            strokeWidth: 0.035, // The thickness
+            color: '#000000' // Fill color
+        },
+         staticLabels: {
+            font: "10px sans-serif",
+            labels: [0,20, 40,80, 100, 120,140,160,180,200],
+            fractionDigits: 0
+          },
+          staticZones: [
+             {strokeStyle: "#FFDD00", min: 0, max: 40},
+             {strokeStyle: "#30B32D", min: 40, max: 60},
+             {strokeStyle: "#F03E3E", min: 60, max: 200}
+          ],
+        limitMax: true,     // If false, max value increases automatically if value > maxValue
+        limitMin: true,     // If true, the min value of the gauge will be fixed
+        colorStart: '#6FADCF',   // Colors
+        colorStop: '#8FC0DA',    // just experiment with them
+        strokeColor: '#E0E0E0',  // to see which ones work best for you
+        // generateGradient: true,
+        highDpiSupport: true,     // High resolution support
+        // renderTicks is Optional
+        renderTicks: {
+            divisions: 5,
+            divWidth: 1.1,
+            divLength: 0.7,
+            divColor: '#333333',
+            subDivisions: 3,
+            subLength: 0.5,
+            subWidth: 0.6,
+            subColor: '#666666'
+        }
+    };
+}
 
-var targetFuelMeter = document.getElementById('fuelMeter'); // your canvas element
-var fuelgauge = new Gauge(targetFuelMeter).setOptions(optsFuelMeter); // create Fuel gauge!                    
-fuelgauge.maxValue = 100; // set max gauge value
-fuelgauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-fuelgauge.animationSpeed = 32; // set animation speed (32 is default value)
-fuelgauge.set(30); // set actual value
+function initGuages(){
+  $.each(CarFuelHistory,
+        function(key, item){
 
-var targetRPMMeter = document.getElementById('RPMMeter'); // your canvas element
-var rpmgauge = new Gauge(targetRPMMeter).setOptions(optsRPMMeter); // create sexy gauge!                    
-rpmgauge.maxValue = 70; // set max gauge value
-rpmgauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-rpmgauge.animationSpeed = 32; // set animation speed (32 is default value)
-rpmgauge.set(30); // set actual value
+            var targetFuelMeter = document.getElementById('fuelMeter-'+ item["VIN"]);
+            var fuelgauge = new Gauge(targetFuelMeter).setOptions(optsFuelMeter);
+            guageControlMap.push({"VIN":item["VIN"], "guage" : fuelgauge , type: "Fuel"});
+            fuelgauge.maxValue = 100;
+            fuelgauge.setMinValue(0);
+            fuelgauge.animationSpeed = 32;
+            fuelgauge.set(item.dashboard["FuelTankLevel"]);
 
-var targetSpeedoMeter = document.getElementById('SpeedoMeter'); // your canvas element
-var speedgauge = new Gauge(targetSpeedoMeter).setOptions(optsSpeedoMeter); // create sexy gauge!                    
-speedgauge.maxValue = 200; // set max gauge value
-speedgauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-speedgauge.animationSpeed = 32; // set animation speed (32 is default value)
-speedgauge.set(30); // set actual value
+            var targetRPMMeter = document.getElementById('RPMMeter-'+ item["VIN"]);
+            var rpmgauge = new Gauge(targetRPMMeter).setOptions(optsRPMMeter);
+            guageControlMap.push({"VIN":item["VIN"], "guage" : rpmgauge , type: "RPM"});
+            rpmgauge.maxValue = 60;
+            rpmgauge.setMinValue(0);
+            rpmgauge.animationSpeed = 32;
+            rpmgauge.set(item.dashboard["RPM"]/100);
+
+            var targetSpeedoMeter = document.getElementById('SpeedoMeter-'+ item["VIN"]);
+            var speedgauge = new Gauge(targetSpeedoMeter).setOptions(optsSpeedoMeter);
+            guageControlMap.push({"VIN":item["VIN"], "guage" : speedgauge , type: "Speed"});
+            speedgauge.maxValue = 200;
+            speedgauge.setMinValue(0);
+            speedgauge.animationSpeed = 32;
+            speedgauge.set(item.dashboard["Speed"]);
+        }
+    );
+}
+
--- Server/dashboard/views.py
+++ Server/dashboard/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.db.models import  Avg, Sum, StdDev
+import json
 
 from obdService.models import CarOBDData, CarProfile
 # Create your views here.
@@ -12,19 +13,24 @@ class DashboardView(View):
         carsWithSpuriousHistory = CarOBDData.objects.values('VIN').filter(PossibleFuelLeak=1).distinct()
         carsWithCleanHistry = CarOBDData.objects.values('VIN').exclude(VIN__in=carsWithSpuriousHistory).distinct()
         carProfiles = list(CarProfile.objects.all())
-        carHistoryProfile = dict()
 
         carsHistry = [dict(item,**{'PossibleFuelLeak':0}) for item in carsWithCleanHistry]
         carsHistry = carsHistry + [dict(item, **{'PossibleFuelLeak': 1}) for item in carsWithSpuriousHistory]
 
         for item in carsHistry:
+            carLastDashboardReading = CarOBDData.objects.filter(VIN=item["VIN"]).values_list('FuelTankLevel',
+                                      'RPM', 'Speed').order_by('-created_at')[:1]
             for profile in carProfiles:
                 if profile.VIN == item["VIN"]:
                     item.update({"Make": profile.Make, "Model": profile.Model})
+                    if carLastDashboardReading  and len(carLastDashboardReading) > 0 :
+                        item["dashboard"] = {'FuelTankLevel': carLastDashboardReading[0][0], 'RPM': carLastDashboardReading[0][1],
+                                             'Speed': carLastDashboardReading[0][2]}
                     break
 
         return render(request, "dashboard/dashboard.html",
                       {
                         #'CarOBDData': CarOBDData.objects.all(),
-                        'CarFuelHistory': carsHistry
+                        'CarFuelHistory': carsHistry,
+                        'jsonCarFuelHistory' : json.dumps(carsHistry)
                       })
