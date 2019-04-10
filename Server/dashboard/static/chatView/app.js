bcfe8f4ffc1c8559c3f5cc044b26846b69253ec9
Rajeev <rajeevmc77@gmail.com>
2019-04-10 13:20:37 +0530

    Added code for Guages

--- Server/dashboard/static/chatView/app.js
+++ Server/dashboard/static/chatView/app.js
@@ -2,11 +2,19 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
+var labels = CarFuelHistory.map(function(value, index){
+    return value[0];
+});
+
+var data = CarFuelHistory.map(function(value, index){
+    return value[1];
+});
+
 var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["1",	"2"	,"3",	"4",	"5",	"6",	"7",	"8",	"9",	"10",	"11",	"12",	"13",	"14",	"15"],
+    labels: labels,
     datasets: [{
       label: "Fuel Level",
       lineTension: 0.3,
@@ -20,9 +28,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [80.9999999803143,	80.9999753215429,	80.9999565100857,	80.9999346494429,	80.9999174657429,	80.9998923474286,	80.9998703726286,	80.9998280656143,	80.9997725204572,	80.9997127246857,	80.9996600267714,
-        	80.9996035340286,	80.9994992679143,	80.9993925713714,	80.9992857448
-      ],
+      data: data,
     }],
   },
   options: {
